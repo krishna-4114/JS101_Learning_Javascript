4b2f7b170553f12848f67ce87c2e1099d0ebfa54
Harikrishna <hari.krishna15499@gmail.com>
2022-10-11 11:16:59 +0530

    Day 1 files added

---
+++ Day 1/problem2.js
@@ -0,0 +1,7 @@
+var name = "Harikrishna Kelwa";
+console.log(name);
+var father_name = "Donger Singh Kelwa";
+console.log(father_name);
+var mother_name = "Sangeeta Kelwa";
+console.log(mother_name);
+
---
+++ Day 1/problem4.js
@@ -0,0 +1,13 @@
+//Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+var name = "Harikrishna Kelwa";
+var school = "San Marino Public School";
+var grade = "A"
+var section = "b"
+var rollno = "124"
+var marks = "300"
+console.log("Name-",name);
+console.log("School-",school);
+console.log("Grade-",grade);
+console.log("Section-",section);
+console.log("Rollno.-",rollno);
+console.log("Total marks-",marks);
